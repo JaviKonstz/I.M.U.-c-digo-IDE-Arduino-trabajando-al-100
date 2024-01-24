4bc7ede5e29280532424a8241f4a8734828b348b
JaviKonstz <157635468+JaviKonstz@users.noreply.github.com>
2024-01-24 13:10:09 -0600

    Add files via upload

---
+++ IMU_TRABAJANDO_AL_100.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <L3G.h>
+
+L3G gyro;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+
+  if (!gyro.init())
+  {
+    Serial.println("Failed to autodetect gyro type!");
+    while (1);
+  }
+
+  gyro.enableDefault();
+}
+
+void loop() {
+  gyro.read();
+  Serial.print((int)gyro.g.x);
+  Serial.print(",");
+  Serial.print((int)gyro.g.y);
+  Serial.print(",");
+  Serial.println((int)gyro.g.z);
+
+  delay(100);
+}
